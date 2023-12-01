03aabd25e8e4d357e5a091efe4fb2d586ac404e7
justinoneill1 <jo05413p@pace.edu>
2023-12-01 18:21:17 -0500

    added scroll function for the nav bar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function redirectToPage() {
     window.location.href = 'https://github.com/justinoneill1/Portfolio'
 }
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if(section) {
+        section.scrollIntoView({behavior: 'smooth'});
+    }
+}
